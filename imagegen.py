af5286d80c9839f069bb9f4ad849a945d45f7055
Rishabh Awasthi <63444778+rishabh-awa@users.noreply.github.com>
2024-11-15 11:38:28 +0530

    Update imagegen.py

--- imagegen.py
+++ imagegen.py
@@ -1,4 +1,5 @@
 import undetected_chromedriver as uc
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
